8892855436a0571b74b3d4df1c9d17db25efb5dc
Onur Ozfuttu <69537532+onurozfuttu@users.noreply.github.com>
2022-11-27 01:33:24 +0300

    Add files via upload

---
+++ promiseEx2.js
@@ -0,0 +1,10 @@
+console.log("This is the first line of code in app.js.");
+
+const usingSTO = () => {
+  console.log('This is middle line');
+}
+
+setTimeout(usingSTO, 3000);
+
+
+console.log("This is the last line of code in app.js.");
